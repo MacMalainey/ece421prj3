9a17ce0f7b694a0e442c2c5fcd596152259623fe
Mackenzie Malainey <macmalainey@gmail.com>
2022-04-07 14:37:59 -0600

    Added conditional migration compilation to server

--- prj3_server/src/main.rs
+++ prj3_server/src/main.rs
@@ -15,12 +15,17 @@ fn rocket() -> _ {
     let rocket = rocket::build()
         .attach(UserDbConn::fairing())
         .mount("/api/v1/", routes::get_routes());
-    
-    let database_url: String = rocket.figment().extract_inner("databases.user_database.url").unwrap();
 
-    let db_conn = SqliteConnection::establish(&database_url).expect("Unable to connect to database for initialization");
+    #[cfg(feature = "build_database")]
+    {
+        println!("Running Database Migration ENABLED (performing now)");
+        let database_url: String = rocket.figment().extract_inner("databases.user_database.url").unwrap();
+        let db_conn = SqliteConnection::establish(&database_url).expect("Unable to connect to database for initialization");
+        shared_types::queries::run_migrations(&db_conn).expect("Database migration failed");
+    }
 
-    shared_types::queries::run_migrations(&db_conn).expect("Database migration failed");
+    #[cfg(not(feature = "build_database"))]
+    println!("Running Database Migration DISABLED (skipping)");
 
     rocket
 }
