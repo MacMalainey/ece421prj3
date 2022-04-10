330041b6fa17809f43315d333d2d91a294475e6d
Mackenzie Malainey <macmalainey@gmail.com>
2022-04-10 17:22:57 -0600

    comment cli

--- prj3_cli/src/types.rs
+++ prj3_cli/src/types.rs
@@ -1,3 +1,4 @@
+/// Main actions user can perform
 pub enum Actions {
     AddUser,
     DeleteUser,
@@ -8,17 +9,20 @@ pub enum Actions {
     Exit
 }
 
+/// Return type for NewUserMenu, contains inputted username and unencrypted password
 pub struct NewUser {
     pub username: String,
     pub password: String
 }
 
+/// Identical to Records<T>, used for List menus
 pub struct PartialList<T> {
     pub items: Vec<T>,
     pub offset: i64,
     pub total: i64
 }
 
+/// Navigation returned from List menus
 pub enum ListNav {
     Next,
     Prev
